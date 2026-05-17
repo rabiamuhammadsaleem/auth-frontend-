7647a349632ea508c0758f2586d467a4837e63a2
rabiamuhammadsaleem <rabiamuhammadsaleem2307@gmail.com>
2026-05-17 13:08:17 +0500

    updated-again

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import api from '../config/axios';
 
 const Signup = () => {
     const [name, setName] = useState('');
